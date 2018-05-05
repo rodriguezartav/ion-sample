5ac7a2266bc48b3e0eaedd55aef2c9071b3a9adc
Roberto <roberto@rodriguezartavia.com>
2018-05-04 21:51:43 -0600

    t

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var ServiceHelper = require("./proton81/service");
 
 
 
+
+
 exports.handler = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   ServiceHelper(event, context, callback)
